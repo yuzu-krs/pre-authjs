9e20abfc78b04fa1a8473983bf137abcc285bb46
yuzu-krs <yuzuto.poi@gmail.com>
2025-03-16 01:12:41 +0900

    プロバイダーを渡さないとAuthJSに遷移する

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -13,7 +13,7 @@ import { SignIn } from "./auth-components";
 
 export default async function UserButton() {
   const session = await auth();
-  if(!session?.user) return <SignIn provider="github" />
+  if(!session?.user) return <SignIn  />
 
   return (
     <div className="flex gap-2 items-center">
