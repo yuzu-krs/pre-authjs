c9a3915dc530670b3c383af0014bb14c80e80957
yuzu-krs <yuzuto.poi@gmail.com>
2025-03-16 01:16:39 +0900

    ログインページにロゴを追加

--- auth.ts
+++ auth.ts
@@ -4,6 +4,9 @@ import Github from "next-auth/providers/github";
 import { JWT } from "next-auth/jwt";
 
 export const config:NextAuthConfig={
+    theme:{
+        logo: "https://media2.dev.to/dynamic/image/width=1080,height=1080,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fmf8mmr9ha1t2oarll8y4.png",
+    },
     providers:[Github({
         clientId:process.env.AUTH_GITHUB_ID,
         clientSecret:process.env.AUTH_GITHUB_SECRET,
